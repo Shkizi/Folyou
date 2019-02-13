fd938c70678a8f97edd158886a41fc08de9e6be5
André Oliveira <andre@outlook.pt>
2019-02-13 17:24:20 +0000

    Update Cards.js

--- src/Components/Elements/Cards/Cards.js
+++ src/Components/Elements/Cards/Cards.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+import { CardContent } from '@material-ui/core';
 
 const styles = {
     card: {
         maxWidth: 300,
         marginTop: 20,
-        marginLeft: 20,
+        marginLeft: 20
     },
     media: {
         // ⚠️ object-fit is not supported by IE 11.
@@ -19,16 +20,18 @@ let n = 10;
 
 function createElements(n){
     return [...Array(n)].map(() => <div>
-    <Card>
+    <Card  className={styles.card}>
+        <CardContent  className={styles.card}>
         <Button>
             Botão
-        </Button>    
+        </Button>   
+        </CardContent> 
     </Card>
 </div>
 )}
 
 
-function Cards(props) {
+function Cards() {
 
     return (
         <div>
