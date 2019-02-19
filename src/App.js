9942f381c8536e9f21ec8216945da4d403f66e7a
André Oliveira <andre@outlook.pt>
2019-02-19 16:08:30 +0000

    Update - Cards design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import Menu from './Components/Pages/Menu/Menu'
-import CardProposal from './Components/Elements/Cards/CardProposal'
-import { Container } from 'react-bootstrap';
-
+import CardProposal from './Components/Elements/Cards/CardProposal/CardProposal'
+import CardPortfolio from './Components/Elements/Cards/CardPortfolio/CardsPortfolio'
 import './App.css'
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 
 class App extends Component {
   render() {
     return (
+          <Container style={{padding: 0, margin: 0}}>
+            <Row>
             <CardProposal/>
-
-
+            </Row>
+          </Container>
     );
   }
 }
