e0b2bad6daea5052332b5d10cdd4246831077f2c
alex-cres <alexrealinho@gmail.com>
2019-02-20 16:33:48 +0000

    Menu Integration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardProposal from './Components/Elements/Cards/CardProposal/CardProposal'
 import CardPortfolio from './Components/Elements/Cards/CardPortfolio/CardsPortfolio'
 import './App.css'
 import { Container, Row, Col } from 'react-bootstrap';
+import Menu from './Components/Menu/Menu';
 
 
 
@@ -10,12 +11,14 @@ import { Container, Row, Col } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
-
+      <div>
+          <Menu/>
           <Container style={{padding: 0, margin: 0}}>
             <Row>
             <CardProposal/>
             </Row>
           </Container>
+          </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
-import Menu from './Components/Menu/Menu';
 import * as serviceWorker from './serviceWorker';
 import { LocalizeProvider } from "react-localize-redux";
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<LocalizeProvider><App /></LocalizeProvider>, document.getElementById('root'));
-ReactDOM.render(<LocalizeProvider><Menu /></LocalizeProvider>, document.getElementById('navbar'));
+ReactDOM.render(<LocalizeProvider><BrowserRouter><App /></BrowserRouter></LocalizeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
