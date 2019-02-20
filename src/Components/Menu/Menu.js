99485d0d75bf231ae7c1b5560d950ab6982ed8fa
Shkizi <andre@outlook.pt>
2019-02-20 23:17:31 +0000

    Master - Navbar now almost finished

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap';
 import './Menu.css'
 import { IoMdMenu } from "react-icons/io";
+import { FiSearch } from "react-icons/fi";
+
+
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Menu = () => (
     <Navbar className="Menu-Navbar" sticky='top'>
-    <Navbar.Brand href="#" className="Menu-Navbar-Brand"><Link to='/'><IoMdMenu/> Menu</Link></Navbar.Brand>
-    <Form inline>
-    <Button variant="outline-info">Search</Button>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    </Form>
-    <h6>Login</h6>
+    <Navbar.Brand href="#"><Link to='/' className="Menu-Navbar-Brand"><IoMdMenu style={{fontSize: "25px"}}/> Menu</Link></Navbar.Brand>  
+    <FiSearch/>
+    <div style={{marginLeft: "41%"}}>
+    <h4>Logotipo</h4>
+    </div>
+    <Nav className="ml-auto">
+    <Link to='/'  className="Menu-Login">
+    <strong>Login</strong></Link>
+    </Nav>
     </Navbar>
   )
   
