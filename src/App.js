c02639740bbde6476369ace2b75a48f5a0554ded
alex-cres <alexrealinho@gmail.com>
2019-02-20 16:49:01 +0000

    Routing Integrations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import CardProposal from './Components/Elements/Cards/CardProposal/CardProposal'
-import CardPortfolio from './Components/Elements/Cards/CardPortfolio/CardsPortfolio'
 import './App.css'
-import { Container, Row, Col } from 'react-bootstrap';
 import Menu from './Components/Menu/Menu';
-
+import Main from './Components/Main/Main';
 
 
 
@@ -13,11 +10,7 @@ class App extends Component {
     return (
       <div>
           <Menu/>
-          <Container style={{padding: 0, margin: 0}}>
-            <Row>
-            <CardProposal/>
-            </Row>
-          </Container>
+          <Main/>
           </div>
     );
   }
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardProposal from '../../../Components/Elements/Cards/CardProposal/CardProposal'
+import CardPortfolio from '../../../Components/Elements/Cards/CardPortfolio/CardsPortfolio'
+import { Container, Row, Col } from 'react-bootstrap';
+const Home = () => (
+
+<Container style={{padding: 0, margin: 0}}>
+            <Row>
+            <CardProposal/>
+            </Row>
+          </Container>
+)
+
+export default Home
