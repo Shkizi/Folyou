b336f303d9ca174870c31894dcfcf434603cded8
alex-cres <alexrealinho@gmail.com>
2019-03-11 14:51:42 +0000

    Menu Language

--- src/Components/Pages/PortfoliosAreaView/PortfoliosAreaView.js
+++ src/Components/Pages/PortfoliosAreaView/PortfoliosAreaView.js
@@ -5,6 +5,7 @@ import CardPortfolio from '../../../Components/Elements/Cards/CardPortfolio/Card
 
 import { Container, Row } from 'react-bootstrap';
 import { withLocalize } from "react-localize-redux";
+import { Translate } from "react-localize-redux";
 
 
 var data;
@@ -12,6 +13,25 @@ const PortfoliosAreaView = () => (
 
     <Container fluid={true}>
         <Row style={{margin: 0}}>
+
+
+ {/* TRENDING SECTION */}
+
+ <Row style={{margin: 0}}>
+                     <Col sm={12} className="Header-Sections">
+                      <h1><Translate id="Portfolios"/></h1>
+                      </Col>
+                      <Col sm={12}>
+                      <hr className="Hr-Sections"/>
+                      <Row>
+                      {jsonPortfolio.map(val =>{return(
+                        <CardPortfolio data={val} />
+                      );})}
+                      </Row>
+                      </Col>
+                    </Row>
+
+
             <CardPortfolio data={data}/>
         </Row>
     </Container>
