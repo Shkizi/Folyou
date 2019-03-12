d66612b0e6e9adb96bf5a0c8094038ec5b6d5976
alex-cres <alexrealinho@gmail.com>
2019-03-12 08:22:55 +0000

    Merge try

--- src/Components/Pages/PortfoliosAreaView/PortfoliosAreaView.js
+++ src/Components/Pages/PortfoliosAreaView/PortfoliosAreaView.js
@@ -6,41 +6,73 @@ import { Container, Row, Col} from 'react-bootstrap';
 import { withLocalize } from "react-localize-redux";
 import { Translate } from "react-localize-redux";
 import jsonPortfolio from '../../Elements/Cards/CardPortfolio/CardPortfolioJSON'
+import serviceAPI from '../../../serviceAPI';
 
-
-var data;
 class PortfoliosAreaView extends React.Component { 
     constructor(props, context) {
         super(props, context);
-    
-        
         this.state = {
-          portfolio:[]
+            portfolios:[],
+            filters:[],
+            hasMoreItems: true,
+            error:[]
         };
       }
+    
+
     componentDidMount() {
-  //axios
-  
-  
+       
+        serviceAPI.get(`getPortfoliosWithFilters`, 
+        { params: {
+              filters: {},
+              offset:0,
+              limit:20
+          }})
+      .then(res => {
+          if(!res.data.error){
+              const portfolios =res.data.portfolios;
+              this.setState({portfolios});
+              const error=[res.data.error,false];
+              this.setState({ error} );
+      console.log(res);
+      
+          }else{
+              const error=[res.data.error,true];
+              this.setState({ error} );
+          }
+      }).catch(errore => {
+        const error=[errore,true];
+        this.setState({ error} );
+      });      
     }
+    
+  
     render() {
+        var items = [];
+        
+        
+        this.state.portfolios.map((portfolio, i) => {
+            items.push(
+               <CardPortfolio data={portfolio}/>
+            );
+        });
+    
+       
       return ( 
     <Container fluid={true}>
-        
-
- {/* TRENDING SECTION */}
-
             <Row style={{margin: 0}}>
-                <Col sm={12} className="Header-Sections">
-                    <h1><Translate id="Portfolio"/></h1>
+                <Col sm={4} className="Header-Sections">
+                    <h1><Translate id="portfolios"/></h1>
+                </Col>
+                <Col sm={8} className="Header-Sections">
+                <h1>Filters Here(Country, Region Search, Keyword[Cat] Search, Per page drop )</h1>
                 </Col>
                 <Col sm={12}>
                     <hr className="Hr-Sections"/>
-                    <Row>
-                        {jsonPortfolio.map(val =>{return(
-                            <CardPortfolio data={val} />
-                        );})}
-                    </Row>
+                </Col>
+                 <Col sm={12}>
+                 {items}
+                  {this.state.error[0]}                 
                 </Col>
             </Row>
             
