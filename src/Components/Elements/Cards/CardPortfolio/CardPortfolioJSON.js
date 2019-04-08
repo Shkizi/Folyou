00add676be2168cfab458bd0179284ff6a46713d
André Oliveira <andre@outlook.pt>
2019-04-08 13:50:46 +0100

    Master - Update Home

--- src/Components/Elements/Cards/CardPortfolio/CardPortfolioJSON.js
+++ src/Components/Elements/Cards/CardPortfolio/CardPortfolioJSON.js
@@ -3,25 +3,24 @@ export default
     {
         "link" : "/Portfolio/1",
         "id":"1",
-        "sheetName" : "Platinum Project",
-        "location" : "Cacém",
-        "name" : "Alexandre Realinho",
-        "description" : "When platinum meats gold",
+        "sheetName" : "Moinhos Eólicos",
+        "name" : "António Maia",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "category" : "Dev",
         "date" : "3 days ago",
-        "avatarImage": "public/anexes/sheets/Portfolio.png",
+        "avatarImage": "public/anexes/sheets/Moinho.png",
         "avatarUser":"public/anexes/profiles/Iade.jpg"
     },
     {
         "link" : "/Portfolio/2",
         "id":"2",
-        "sheetName" : "Pokemon Creatures",
+        "sheetName" : "Como funciona a ai? ",
         "location" : "Madagascar",
-        "name" : "André Oliveira",
-        "description" : "New pokemons i created with Illustrator",
+        "name" : "Manuel Letras",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
         "category" : "Dev",
         "date" : "19 days ago",
-        "avatarImage": "",
+        "avatarImage": "public/anexes/sheets/Ai.jpg",
         "avatarUser":""
     }
 
--- src/Components/Elements/Cards/CardTalent/CardTalentJSON.js
+++ src/Components/Elements/Cards/CardTalent/CardTalentJSON.js
@@ -2,20 +2,21 @@ export default
 [
     {
         "link" : "/Talent/1",
+        "offerName" : "Especialista em Java",
         "name" : "Alexandre Realinho",
         "location" : "Cacém",
         "website" : "github/alexrealinho",
-        "description" : "User owner description",
+        "description" : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet",
         "category" : "Rising Star",
         "avatarUser":""
     },
     {
         "link" : "/Talent/2",
-        "offerName" : "Junior Front-End Developer",
+        "offerName" : "Photoshop Ninja",
         "location" : "Porto",
         "name" : "José Mário",
-        "description" : "Proposal description Proposal description Proposal description Proposal description Proposal description Proposal description Proposal description Proposal description ",
-        "category" : "Dev",
+        "description" : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni sit amet",
+        "category" : "Rising Star",
         "date" : "19 days ago",
         "avatarUser":"public/anexes/profiles/Iade.jpg"
     },
