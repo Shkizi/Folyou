8f2c6fcd3c048d50476daae0f89632f9522cbbb4
André Oliveira <andre@outlook.pt>
2019-06-04 23:40:15 +0100

    Master - Carrousel pronto para o backend

--- src/Components/Elements/Carousel/HomeCarousel/HomeCarousel.js
+++ src/Components/Elements/Carousel/HomeCarousel/HomeCarousel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Carousel} from 'react-bootstrap';
+import { Carousel, Image, Col, Row,} from 'react-bootstrap';
 
 import ServicesAPI from "../../../../serviceAPI";
 import './HomeCarousel.css'
@@ -12,24 +12,38 @@ class HomeCarousel extends React.Component {
     render() {
         return ( 
        
-            <Carousel className="Home-Carousel" >
-            {this.props.parent.state.portTrending.concat(this.props.parent.state.portfolios).map(val =>{return(
+            <Carousel className="Home-Carousel">
+        {/*     {this.props.parent.state.portTrending.concat(this.props.parent.state.portfolios).map(val =>{return(
                         <Carousel.Item className="HomeCarousel-Item">
                         <img
                           className="d-block w-100 HomeCarousel-Image"
                           src={S.baseURL()+"public/anexes/sheets/"+val.defaultImageSheet}
                           alt="First slide"
-                        />
-                        
-                        
-                        <Carousel.Caption style={{color:"black",backgroundColor: "rgba(147, 147, 147,0.4)"}}>
-                          <b><h4>{val.nameSheet}</h4></b>
-                          <p>{val.descriptionSheet}</p>
-                        </Carousel.Caption>
-                      </Carousel.Item>
-                      );})}
-           
-           
+                        /> */}
+
+                           <Carousel.Item className="HomeCarousel-Item">
+                              <div className="w-100 HomeCarrousel-Background">
+                                <Row>
+                                <Col md="5" lg="5">
+                                    <div className="container HomeCarrousel-Image-Container">
+                                      <Image className="HomeCarousel-Image" src="https://rockcontent.com/wp-content/uploads/2017/01/formatos-de-imagem-2.jpg" thumbnail />
+                                    </div>
+                                </Col>
+                                <Col md="7" lg="7">
+                                    <div className="HomeCarrousel-Text-Container">
+                                      <div className="HomeCarrousel-Text-Project-Name">Project Name</div>
+                                      <div className="HomeCarrousel-Text-Project-Origin">Country, region</div>
+                                      <div className="HomeCarrousel-Text-Project-Description">
+                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST 
+                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST 
+                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST
+                                      </div>
+                                    </div>
+                                </Col>
+                                </Row>
+                              </div>
+                              
+                           </Carousel.Item>           
            
           </Carousel>
         );} }
