306e339ec651db1843e062937ac9d3cfa58ebb67
alex-cres <alexrealinho@gmail.com>
2019-06-05 15:09:17 +0100

    Carrosel Fixed

--- src/Components/Elements/Carousel/HomeCarousel/HomeCarousel.js
+++ src/Components/Elements/Carousel/HomeCarousel/HomeCarousel.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Carousel, Image, Col, Row,} from 'react-bootstrap';
 
 import ServicesAPI from "../../../../serviceAPI";
+import getImageLanguage from "../../../../Resources/Translations/compilerLanguageImages.js"
 import './HomeCarousel.css'
 
 
@@ -13,39 +14,36 @@ class HomeCarousel extends React.Component {
         return ( 
        
             <Carousel className="Home-Carousel">
-        {/*     {this.props.parent.state.portTrending.concat(this.props.parent.state.portfolios).map(val =>{return(
-                        <Carousel.Item className="HomeCarousel-Item">
-                        <img
-                          className="d-block w-100 HomeCarousel-Image"
-                          src={S.baseURL()+"public/anexes/sheets/"+val.defaultImageSheet}
-                          alt="First slide"
-                        /> */}
-
-                           <Carousel.Item className="HomeCarousel-Item">
+             {this.props.parent.state.portTrending.concat(this.props.parent.state.portfolios).map(val =>{return(
+            
+            <Carousel.Item className="HomeCarousel-Item">
                               <div className="w-100 HomeCarrousel-Background">
                                 <Row>
                                 <Col xs="5" sm="5" md="5" lg="5" xl="5">
                                     <div className="container HomeCarrousel-Image-Container">
-                                      <Image className="HomeCarousel-Image" src="https://rockcontent.com/wp-content/uploads/2017/01/formatos-de-imagem-2.jpg" thumbnail />
+                                      <Image className="HomeCarousel-Image" src={S.baseURL()+"public/anexes/sheets/"+val.defaultImageSheet} thumbnail />
                                     </div>
                                 </Col>
                                 <Col xs="7" sm="7" md="7" lg="7" xl="7">
                                     <div className="HomeCarrousel-Text-Container">
-                                      <div className="HomeCarrousel-Text-Project-Name">Project Name</div>
-                                      <div className="HomeCarrousel-Text-Project-Origin">Country, region</div>
+                                      <div className="HomeCarrousel-Text-Project-Name">{val.nameSheet}</div>
+                                      <div className="HomeCarrousel-Text-Project-Origin">
+                                      <Image src={getImageLanguage(val.countrySheet)} style={{height: "32px",width: "32px"}} roundedCircle/>{val.regionSheet} 
+                                        </div>
                                       <div className="HomeCarrousel-Text-Project-Description">
-                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST 
-                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST 
-                                      DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST DESCRIPTION TEST
+                                      {val.descriptionSheet.substring(0,200)+((val.descriptionSheet.length > 200)?"...":"")}
                                       </div>
                                     </div>
                                 </Col>
                                 </Row>
                               </div>
                               
-                           </Carousel.Item>           
-           
-          </Carousel>
-        );} }
-
+              </Carousel.Item>  
+            
+            
+            
+            )} )}
+        </Carousel>
+        )}
+}
 export default HomeCarousel;
