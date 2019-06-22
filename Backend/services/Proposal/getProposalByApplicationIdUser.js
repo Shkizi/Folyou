e3abb15f4be71f2816f267368167e7e308889a6c
alex-cres <alexrealinho@gmail.com>
2019-06-22 15:12:42 +0100

    Anexes in the card modal done

--- Backend/services/Proposal/getProposalByApplicationIdUser.js
+++ Backend/services/Proposal/getProposalByApplicationIdUser.js
@@ -7,7 +7,9 @@ var db = require('../dbconnect.js');
 function getProposalByApplicationIdUser(req, res, next) {
     let params = req.query;
 
-    db.query("SELECT * FROM applicationstate,proposalstate, User  RIGHT JOIN applicationteamuser    ON user.idUser = applicationteamuser.User_idUser    RIGHT JOIN applicationteam    ON applicationteam.idApplicationTeam = applicationteamuser.ApplicationTeam_idApplicationTeam    RIGHT JOIN application    ON applicationteam.idApplicationTeam = application.ApplicationTeam_idApplicationTeam    RIGHT JOIN proposal    ON proposal.idProposal = application.Proposal_idProposal   WHERE idUser = ? AND proposalstate.idProposalState = proposal.ProposalState_idProposalState AND applicationstate.idApplicationState = application.ApplicationState_idApplicationState; ",[parseInt(params.idUser)] , function (rows, error) {
+    db.query("SELECT * FROM applicationstate,proposalstate, User  RIGHT JOIN applicationteamuser    ON user.idUser = applicationteamuser.User_idUser    RIGHT JOIN applicationteam    ON applicationteam.idApplicationTeam = applicationteamuser.ApplicationTeam_idApplicationTeam    RIGHT JOIN application    ON applicationteam.idApplicationTeam = application.ApplicationTeam_idApplicationTeam    RIGHT JOIN proposal    ON proposal.idProposal = application.Proposal_idProposal  "+
+     "RIGHT JOIN (SELECT Proposal_idProposal, GROUP_CONCAT( fileName) fil FROM anexes WHERE Proposal_idProposal IS NOT NULL GROUP BY Proposal_idProposal ) AS anexedfiles ON " + " anexedfiles.Proposal_idProposal = idProposal "+
+    "WHERE idUser = ? AND proposalstate.idProposalState = proposal.ProposalState_idProposalState AND applicationstate.idApplicationState = application.ApplicationState_idApplicationState; ",[parseInt(params.idUser)] , function (rows, error) {
         console.log(params);
         if (!error) {
             console.log(rows);
